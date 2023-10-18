32f84e5e98d938b8a4aee63c2a605982bd78cd02
Gustavo <gustavo-adsilva26@educar.rs.gov.br>
2023-10-18 11:47:24 -0300

    :zap: Ajustes na barra de navegação

--- src/componente/header/index.js
+++ src/componente/header/index.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import SearchBar from "./searchBar/index"; // Importe o componente SearchBar
+import SearchBar from "./searchBar/index";
 import "./header.css";
 
 function Header() {
   const [active, setActive] = useState("nav__menu");
   const [icon, setIcon] = useState("nav__toggler");
+
   const navToggle = () => {
     if (active === "nav__menu") {
       setActive("nav__menu nav__active");
@@ -25,44 +26,45 @@ function Header() {
   };
 
   return (
-    <nav className="nav">
-     <h1 className="Magic">ReelMagic</h1>
-     <SearchBar/> 
-      <ul className={active}>
-        <li className="nav__item">
-          <a href="/" className="nav__link" onClick={closeMenu}>
-            Home
-          </a>
-        </li>
+    <div>
+      <nav className="nav">
+        <h1 className="Magic">ReelMagic</h1>
+        <ul className={active}>
           <li className="nav__item">
-          <a href="/favoritos" className="nav__link" onClick={closeMenu}>
-            Minha Lista
-          </a>
-        </li>
-        <li className="nav__item">
-          <a href="#filmes" className="nav__link" onClick={closeMenu}>
-            Filmes
-          </a>
-        </li>
-        <li className="nav__item">
-          <a href="#planos" className="nav__link" onClick={closeMenu}>
-            Planos
-          </a>
-        </li>
-        <li className="nav__item">
-          <a href="#contatos" className="nav__link" onClick={closeMenu}>
-            Contatos
-          </a>
-        </li>   
-      </ul> 
-     
-      <div onClick={navToggle} className={icon}>
-       
-        <div className="lineum"></div>
-        <div className="linedois"></div>
-        <div className="linetres"></div>
-      </div>
-    </nav>
+            <a href="/" className="nav__link" onClick={closeMenu}>
+              Home
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="/favoritos" className="nav__link" onClick={closeMenu}>
+              Minha Lista
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#filmes" className="nav__link" onClick={closeMenu}>
+              Filmes
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#planos" className="nav__link" onClick={closeMenu}>
+              Planos
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#contatos" className="nav__link" onClick={closeMenu}>
+              Contatos
+            </a>
+          </li>
+        </ul>
+        <SearchBar />
+        <div onClick={navToggle} className={icon}>
+          <div className="lineum"></div>
+          <div className="linedois"></div>
+          <div className="linetres"></div>
+        </div>
+      </nav>
+      
+    </div>
   );
 }
 
